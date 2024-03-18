5e9b2f99634fb6257e551283d876bf595a848c89
olugon <126735508+olugon0821@users.noreply.github.com>
2024-03-18 11:35:22 +0900

    8LEDS.ino

--- week7/8LEDS.ino
+++ week7/8LEDS.ino
@@ -1,22 +1,22 @@
-const uint8_t LED_Pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
-const uint8_t LEDNUM = sizeof(LED_Pins) / sizeof(LED_Pins[0]);
+const uint8_t LED_Pins[] {2U, 3U, 4U, 5U, 6U, 7U, 8U, 9U};
+
 
 void setup() {
   // put your setup code here, to run once:
-  for (int i = 0; i < LEDNUM; ++i) {
+  for (int i = 0; i < 8; ++i) {
     pinMode(LED_Pins[i], OUTPUT);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i < LEDNUM; ++i) {
+  for (int i = 0; i < 8; ++i) {
     digitalWrite(LED_Pins[i], HIGH);
-    delay(1000UL);
+    delay(500UL);
   }
 
-  for (int i = LEDNUM - 1; i >= 0; --i) {
+  for (int i = 7; i >= 0; --i) {
     digitalWrite(LED_Pins[i], LOW);
-    delay(1000UL);
+    delay(500UL);
   }
 }
